a19bb9f2ed4860eec7bb6c41f415b411f670af74
pjsier <pjsier@gmail.com>
2020-07-08 11:00:07 -0500

    Updates

---
+++ precache-manifest.2d3c299a0398dc89b8d7c49146b42469.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0364fa437801bc2cc7f7397c3f819d49",
+    "url": "/city-scrapers-events/index.html"
+  },
+  {
+    "revision": "8d4a27a514c9cbe44532",
+    "url": "/city-scrapers-events/static/css/2.38e55c77.chunk.css"
+  },
+  {
+    "revision": "024ee6a25919a98b3eb2",
+    "url": "/city-scrapers-events/static/css/main.61e0a910.chunk.css"
+  },
+  {
+    "revision": "8d4a27a514c9cbe44532",
+    "url": "/city-scrapers-events/static/js/2.c5c2fb7a.chunk.js"
+  },
+  {
+    "revision": "95aed6a00765aeb803e861408bf7612d",
+    "url": "/city-scrapers-events/static/js/2.c5c2fb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024ee6a25919a98b3eb2",
+    "url": "/city-scrapers-events/static/js/main.5a85a7ed.chunk.js"
+  },
+  {
+    "revision": "0c81663d2f127d2c85bd",
+    "url": "/city-scrapers-events/static/js/runtime-main.fd9e89bd.js"
+  },
+  {
+    "revision": "0d6bbdaf2a0072f6e26583a1ab836c76",
+    "url": "/city-scrapers-events/static/media/logo.0d6bbdaf.png"
+  }
+]);
